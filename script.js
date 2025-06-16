60516cbce8a61090a5ae036bf6688b9a8e5ac697
sametefebaydar <sametefebydae@gmail.com>
2025-06-16 09:38:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function girisYap() {
-  const sifre = document.getElementById("sifre").value;
-  if (sifre === "250805Seb.") {
-    window.location.href = "panel.html";
-  } else {
-    alert("Şifre yanlış. Lütfen tekrar deneyin.");
+<script>
+  function girisYap() {
+    const sifre = document.getElementById("sifre").value;
+    const hata = document.getElementById("hataMesaji");
+    if (sifre === "250805Seb.") {
+      window.location.href = "panel.html";
+    } else {
+      hata.style.display = "block";
+    }
   }
-}
+</script>
