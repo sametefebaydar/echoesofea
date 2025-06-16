ddb885396b9bb30d690b4fdc43493bdeb1cc08c1
sametefebaydar <sametefebydae@gmail.com>
2025-06-16 09:01:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function girisYap() {
   const sifre = document.getElementById("sifre").value;
   if (sifre === "250805Seb.") {
-    document.getElementById("giris-ekrani").style.display = "none";
-    document.getElementById("ana-panel").style.display = "block";
+    window.location.href = "panel.html";
   } else {
     alert("Şifre yanlış. Lütfen tekrar deneyin.");
   }
